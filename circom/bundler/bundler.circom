ff5581cac07a0b89488f83dcd9e1d46d81081c67
ZeroKPunk <jusonxia@gmail.com>
2022-12-27 17:48:24 +0800

    update

--- circom/bundler/bundler.circom
+++ circom/bundler/bundler.circom
@@ -1,5 +1,43 @@
 pragma circom 2.0.0;
 
-template BundlerCore() {
-  
+include "../../node_modules/circomlib/circuits/eddsaposeidon.circom";
+include "../../node_modules/circomlib/circuits/poseidon.circom";
+
+
+template BundlerVerifier(nInputs) {
+
+  signal input userOps[nInputs];
+
+  signal input enabled[nInputs];
+  signal input Axs[nInputs];
+  signal input Ays[nInputs];
+  signal input R8x[nInputs];
+  signal input R8y[nInputs];
+  signal input S[nInputs];
+  signal input M[nInputs];
+
+  signal output userOpsHash;
+  component signVerifiers[nInputs];
+
+  for(var i=0; i < nInputs; i++) {
+    signVerifiers[i] = EdDSAPoseidonVerifier();
+  }
+
+  component hash = Poseidon(nInputs);
+  for(var i=0; i < nInputs; i++) {
+    
+    signVerifiers[i].enabled <== enabled[i];
+    signVerifiers[i].Ax <== Axs[i];
+    signVerifiers[i].Ay <== Ays[i];
+    signVerifiers[i].R8x <== R8x[i];
+    signVerifiers[i].R8y <== R8y[i];
+    signVerifiers[i].S <== S[i];
+    signVerifiers[i].M <== M[i];
+
+    hash.inputs[i] <== userOps[i];
+
+  }
+
+  userOpsHash <== hash.out;
+
 }
---
+++ test/bundler.test.js
@@ -0,0 +1,79 @@
+const buildEddsa = require("circomlibjs").buildEddsa;
+const buildBabyjub = require("circomlibjs").buildBabyjub;
+const wasm_tester = require("circom_tester").wasm;
+
+const chai = require("chai");
+const path = require("path");
+const assert = chai.assert;
+
+describe('Bundler Test', function() {
+  this.timeout(1000000);
+  before(async() => {
+    eddsa = await buildEddsa();
+    babyJub = await buildBabyjub();
+    F = babyJub.F;
+    circuit = await wasm_tester(path.join(__dirname, "circom", "bundler_test.circom"));
+    eddsaPoseidonCircuit = await wasm_tester(path.join(__dirname, "circom", "eddsaposeidon_test.circom"));
+    multiplierCircuit = await wasm_tester(path.join(__dirname, "../circom/multiplier2", "multiplier2.circom"));
+
+  });
+  it('test eddsa posidon', async () => {
+    const msg = F.e(1234);
+
+    const prvKey = Buffer.from("0001020304050607080900010203040506070809000102030405060708090001", "hex");
+
+    const pubKey = eddsa.prv2pub(prvKey);
+
+    const signature = eddsa.signPoseidon(prvKey, msg);
+
+    assert(eddsa.verifyPoseidon(msg, signature, pubKey));
+
+    const input = {
+        enabled: 1,
+        Ax: F.toObject(pubKey[0]),
+        Ay: F.toObject(pubKey[1]),
+        R8x: F.toObject(signature.R8[0]),
+        R8y: F.toObject(signature.R8[1]),
+        S: signature.S,
+        M: F.toObject(msg)
+    };
+
+    // console.log(JSON.stringify(utils.stringifyBigInts(input)));
+
+    const w = await eddsaPoseidonCircuit.calculateWitness(input, true);
+    // console.log(`witness1= ${JSON.stringify(w)}`)
+
+    await eddsaPoseidonCircuit.checkConstraints(w);
+  });
+
+  it('base test', async () => {
+    const msg = F.e(1234);
+    console.log(`msg ${JSON.stringify(msg)}`)
+
+    const prvKey = Buffer.from("0001020304050607080900010203040506070809000102030405060708090001", "hex");
+
+    const pubKey = eddsa.prv2pub(prvKey);
+    console.log(`pubKey ${pubKey}`)
+
+    const signature = eddsa.signPoseidon(prvKey, msg);
+    console.log(`signature ${JSON.stringify(signature)}`)
+
+    assert(eddsa.verifyPoseidon(msg, signature, pubKey));
+
+    const input = {
+      enabled: 1,
+      userOps: F.toObject(msg),
+      Axs: F.toObject(pubKey[0]),
+      Ays: F.toObject(pubKey[1]),
+      R8x: F.toObject(signature.R8[0]),
+      R8y: F.toObject(signature.R8[1]),
+      S: signature.S,
+      M: F.toObject(msg),
+    }
+    
+    const w = await circuit.calculateWitness(input,true)
+    // console.log(`witness ${JSON.stringify(w)}`)
+    await circuit.checkConstraints(w);
+
+  });
+})
