11e98e5be716660140677b0447fcd72c47273ccc
ZeroKPunk <jusonxia@gmail.com>
2022-12-29 10:48:46 +0800

    update automation scripts

--- circom/bundler/001_config_pot.sh
+++ circom/bundler/001_config_pot.sh
@@ -1,16 +1,16 @@
 mkdir -p tmp_powers_of_tao
 
-cd tmp_powers_of_tao
+cd tmp_powers_of_tao25
 
-snarkjs powersoftau new bn128 19 pot19_0000.ptau -v
+snarkjs powersoftau new bn128 25 pot25_0000.ptau -v
 
-snarkjs powersoftau contribute pot19_0000.ptau pot19_0001.ptau --name="First contribution" -v
+snarkjs powersoftau contribute pot25_0000.ptau pot25_0001.ptau --name="First contribution" -v
 
-snarkjs powersoftau contribute pot19_0000.ptau pot19_0002.ptau --name="Second contribution" -v -e="some random text"
+snarkjs powersoftau contribute pot25_0000.ptau pot25_0002.ptau --name="Second contribution" -v -e="some random text"
 
 # take a long time ...
-snarkjs powersoftau beacon pot19_0002.ptau pot19_beacon.ptau 0101930405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f 10 -n="Final Beacon"
+snarkjs powersoftau beacon pot25_0002.ptau pot25_beacon.ptau 0102530405060708090a0b0c0d0e0f101112131415161718251a1b1c1d1e1f 10 -n="Final Beacon"
 
-snarkjs powersoftau prepare phase2 pot19_beacon.ptau pot19_final.ptau -v
+snarkjs powersoftau prepare phase2 pot25_beacon.ptau pot25_final.ptau -v
 
-snarkjs powersoftau verify pot19_final.ptau
+snarkjs powersoftau verify pot25_final.ptau
---
+++ circom/bundler/002_execute_groth16.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+mkdir -p tmp_groth16_zk_process
+cd tmp_groth16_zk_process
+
+compile_start=`date +%s.%N`
+# Compile the circuit
+circom ../bundler.circom --r1cs --wasm --sym --c
+
+compile_end=`date +%s.%N`
+
+compile_runtime=$( echo "$compile_end - $compile_start" | bc -l )
+
+echo "=============compile_runtime $compile_runtime"
+
+# Copy the input file inside the sudoku_js directory
+cp ../input.json bundler_js/input.json
+
+
+generatezk_start=`date +%s.%N`
+# Generate a .zkey file that will contain the proving and verification keys together with all phase 2 contributions
+snarkjs groth16 setup bundler.r1cs ../tmp_powers_of_tao/pot19_final.ptau circuit_final.zkey
+
+# Export the zkey
+
+snarkjs zkey export verificationkey circuit_final.zkey verification_key.json
+
+generatezk_end=`date +%s.%N`
+generatezk_runtime=$( echo "$generatezk_end - $generatezk_start" | bc -l )
+echo "=============generatezkey_runtime $generatezk_runtime"
+
+
+prove_start=`date +%s.%N`
+# Go inside the sudoku_js directory and generate the witness.wtns
+cd bundler_js
+node generate_witness.js bundler.wasm input.json witness.wtns
+
+# Copy the witness.wtns to the outside and go there
+cp witness.wtns ../witness.wtns
+cd ..
+# Create the Proof 
+snarkjs groth16 prove circuit_final.zkey witness.wtns proof.json public.json
+
+prove_end=`date +%s.%N`
+prove_runtime=$( echo "$prove_end - $prove_start" | bc -l )
+echo "=============prove_runtime $prove_runtime"
+
+# Verify
+snarkjs groth16 verify verification_key.json public.json proof.json
